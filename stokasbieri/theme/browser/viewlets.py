3c8227fc632b0049932189470a3d82b3ee2dbb93
Kurt Bendl <kurt@tool.net>
2010-10-18 08:16:09 -0600

    renaming files - methods to stokasbieri throughout.

--- stokasbieri/theme/browser/viewlets.py
+++ stokasbieri/theme/browser/viewlets.py
@@ -9,23 +9,23 @@ import DateTime
 class SearchBoxViewlet(common.SearchBoxViewlet):
     """A customixed version of the searchbox
     """
-    index = ViewPageTemplateFile('bieri_searchbox.pt')
+    index = ViewPageTemplateFile('stokasbieri_searchbox.pt')
 
 
-class BieriPictureBoxViewlet(ViewletBase):
+class StokasBieriPictureBoxViewlet(ViewletBase):
     """A viewlet that contains pictures and some info
     """
-    index = ViewPageTemplateFile('bieri_picturebox.pt')
+    index = ViewPageTemplateFile('stokasbieri_picturebox.pt')
 
 
-class BieriGlobalSectionsViewlet(common.GlobalSectionsViewlet):
+class StokasBieriGlobalSectionsViewlet(common.GlobalSectionsViewlet):
     """override of the somewhat lacking GlobalSectionsViewlet
     """
     @memoize
-    def bieriDate(self):
+    def stokasbieriDate(self):
         now = DateTime.DateTime()
         foo = "%s, %s %d, %d" % (now.Day(), now.Month(), now.day(), now.year())
         #import pdb; pdb.set_trace()
         return foo
 
-    index = ViewPageTemplateFile('bieri_globalnav.pt')
+    index = ViewPageTemplateFile('stokasbieri_globalnav.pt')
